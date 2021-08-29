68174a6ba691f3e751fb193ef3eb496b392a7c26
jzarzeczny <jkzarzeczny@gmail.com>
2021-08-29 21:07:25 +0200

    working on pc res

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,24 +1,21 @@
 import React from "react";
+import Layout from "../components/Layout";
 import Features from "../components/Features";
-import Header from "../components/Header";
 import Intro from "../components/Intro";
 import Services from "../components/Services";
 import Options from "../components/Opinions";
 import ArrowNav from "../components/ArrowNav";
 import Articles from "../components/Articles";
-import Footer from "../components/Footer";
 
 export default function Main() {
   return (
-    <>
-      <Header />
+    <Layout>
       <Intro />
       <Services />
       <Features />
       <Options />
       <ArrowNav />
       <Articles />
-      <Footer />
-    </>
+    </Layout>
   );
 }
